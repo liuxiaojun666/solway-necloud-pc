d565c0cd4b3ad0a62b758de8fa9046cca0236fce
liu xiaojun <lxj_web@163.com>
2018-09-07 19:07:12 +0800

    update

--- solway_necloud_es6/myJs/enrichment/monitoringSummary/components/themeBasicSituation/vedio.js
+++ solway_necloud_es6/myJs/enrichment/monitoringSummary/components/themeBasicSituation/vedio.js
@@ -10,7 +10,7 @@ ajaxData({
         $scope.selectVideos.subscribe((res) => {
             $scope.params = [];
             const $videosContainer = $('#videosContainer');
-            res.videos.forEach(item => {
+            res.videos.forEach((item, i) => {
                 $scope.params.push({
                     station_id: item.stId,
                     stream: [
@@ -23,30 +23,24 @@ ajaxData({
                 });
                 $videosContainer.append(
                     `<embed src="vendor/flash/videoPlayer.swf" 
+                        style="float: ${(i % 2) === 0 ? 'left' : 'right'}"
                         width="640" 
                         height="377"
+                        controls=console
                         quality="high" 
                         bgcolor="#000000"
                         name="videoPlayer" 
                         allowfullscreen="true" 
                         pluginspage="http://www.adobe.com/go/getflashplayer" 
-                        flashvars="&amp;videoWidth=0&amp;videoHeight=0&amp;dsControl=manual&amp;dsSensitivity=100&amp;serverURL=${item.monitorPointDest}&amp;DS_Status=true&amp;streamType=live&amp;autoStart=false"
+                        flashvars="&amp;videoWidth=0&amp;videoHeight=0&amp;dsControl=manual&amp;dsSensitivity=100&amp;serverURL=${item.monitorPointDest}&amp;DS_Status=true&amp;streamType=live&amp;autoStart=true"
                         type="application/x-shockwave-flash" />`
                 );
             });
-            // $scope.params.forEach(item => {
-            //     $http({
-            //         method: "POST",
-            //         url: $scope.selectVideos.res.VIDEO_URL,
-            //         timeout: 4000,
-            //         headers: {
-            //             'Content-Type': 'application/x-www-form-urlencoded'
-            //         },
-            //         data: item
-            //     });
-            // });
-            // $scope.setInterval();
+            $scope.setInterval();
         });
+
+
+        
         let timer;
         $scope.setInterval = function () {
             timer = setInterval(function () {
@@ -56,9 +50,10 @@ ajaxData({
                         url: $scope.selectVideos.res.VIDEO_URL,
                         timeout: 4000,
                         headers: {
-                            'Content-Type': 'application/x-www-form-urlencoded'
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                            // "Access-Control-Allow-Origin": "*"
                         },
-                        data: item
+                        data: item,
                     });
                 });
             }, 5000);
