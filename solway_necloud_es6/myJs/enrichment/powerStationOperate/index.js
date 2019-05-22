8eb8c3642b1b79c8fd26e07a314b20c28fe2a09a
dudapeng <343025610@qq.com>
2019-05-22 11:27:55 +0800

    专家分析

---
+++ solway_necloud_es6/myJs/enrichment/powerStationOperate/index.js
@@ -0,0 +1,6 @@
+ajaxData({
+    
+}, {})('powerStationOperateCtrl', ['$scope', 'myAjaxData', 'actionRecord', '$timeout','toaster','$interval'], ($scope, myAjaxData, historicalRecord, $timeout,toaster,$interval) => {
+    $scope.moduleName = '电站运行';//当前页面名称；
+
+});
